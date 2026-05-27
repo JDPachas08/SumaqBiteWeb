4772510a53f9d36c1f6b164808c0b6cdd199270c
JDPachas08 <183675790+JDPachas08@users.noreply.github.com>
2026-05-27 08:13:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -475,7 +475,7 @@ async function procesarChatQuini() {
     try {
 
         const model = ai.getGenerativeModel({
-            model: 'gemini-2.0-flash'
+            model: 'gemini-1.5-flash-8b'
         });
 
         const prompt = `
